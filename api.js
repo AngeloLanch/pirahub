6189697519abdbfe7974dad022b6419aaf2c5fb7
Angelo <angelo.pjsol@gmail.com>
2020-10-26 14:11:14 -0300

    api

--- api.js
+++ api.js
@@ -5,3 +5,27 @@ async function getUser() {
 
     console.log(result);
 }
+
+function userList() {
+    const resultsBox = document.getElementsById('resultsBox');
+    
+    result.items.forEach(user => {
+        <div class="userBox">
+            <div class="userInfos">
+                <div class="userImage">
+                    <img src="./img/perfil.png"/>
+                </div>
+                <div class="userName">
+                    <p>user name</p>
+                </div>
+            </div>
+            <div class="userDescription">
+                <p>bla blab lab bla bla bla bl bla bla bla esse usuário é top demais! </p>
+            </div>
+        </div>
+        const div = resultsBox.appendChild(document.createElement('div'));
+        div.    
+    })
+    
+
+}
